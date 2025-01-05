5ea0408bf0823c9d451c3b82efc33dc4defcbd02
heyEdem <akatoaugustine@gmail.com>
2025-01-05 14:15:23 +0000

    initial commit

---
+++ src/main/java/com/edem/medlink/MedlinkApplication.java
@@ -0,0 +1,13 @@
+package com.edem.medlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedlinkApplication.class, args);
+    }
+
+}
